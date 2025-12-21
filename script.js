ae20cfe9c62dfa84d3d9cd1408ec1a3aff948d58
Maor Sharon <maor@zappy5.com>
2025-12-21 11:42:28 +0200

    Pre-publish sync: Fix paths for sub-pages

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const phoneHeaderBtn = document.querySelector('.phone-header-btn');
   if (phoneHeaderBtn) {
     phoneHeaderBtn.addEventListener('click', function() {
-      const phoneNumber = '[business_phone]';
+      const phoneNumber = '+972 - 54 - 6456581';
       window.location.href = 'tel:' + phoneNumber;
     });
   }
